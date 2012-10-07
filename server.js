4e9c2a0337ecba178f72e3a49f82008b42a6655c
marcus <marc-olivierricard@whitelion.local>
2012-10-07 12:32:29 -0400

    init

---
+++ server.js
@@ -0,0 +1,31 @@
+var application_root = __dirname,
+    express = require("express"),
+    path = require("path"),
+    mongoose = require('mongoose');
+
+var app = express();
+
+// Database
+
+mongoose.connect('mongodb://localhost/savonettes');
+
+// Config
+
+app.configure(function () {
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(application_root, "public")));
+    app.use(express.errorHandler({ 
+        dumpExceptions: true, 
+        showStack: true 
+    }));
+});
+
+app.get('/api', function (req, res) {
+    res.send('Status: running');
+});
+
+// Launch server
+
+app.listen(4242);
